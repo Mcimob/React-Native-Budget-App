a9a150968e51f22118b969d0fb9b988002ecf0da
mcimob <burckhardttim@gmail.com>
2023-05-08 17:36:51 +0200

    Added bottom navigation

--- App.js
+++ App.js
@@ -6,29 +6,53 @@
  */
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import HomeScreen from './Home';
 import AddPage from './AddPage';
 import AllWalletsPage from './AllWalletsPage';
-import {Button} from 'react-native-paper';
+import {Icon} from './Icon';
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="AddPage" component={AddPage} />
-        <Stack.Screen
-          name="ViewWallets"
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focussed, color, size}) => {
+            let iconName;
+            let iconSource = 'ant';
+            switch (route.name) {
+              case 'Home':
+                iconName = 'home';
+                break;
+              case 'AddPage':
+                iconName = 'pluscircleo';
+                break;
+              case 'All Wallets':
+                iconName = 'wallet';
+                break;
+              case 'All Categories':
+                iconName = 'category';
+                iconSource = 'material';
+                break;
+            }
+            return (
+              <Icon type={iconSource} name={iconName} size={20} color="black" />
+            );
+          },
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="AddPage" component={AddPage} />
+        <Tab.Screen
+          name="All Wallets"
           component={AllWalletsPage}
           options={{headerRight: () => {}}}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default App;
